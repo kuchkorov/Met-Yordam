9d209bac0f113651832d0b5009dbda2eb1491237
Mamur Kuchkorov <kuchkorov.mamurjon@mail.ru>
2024-05-08 09:45:55 +0500

    left content updated

--- src/components/main-content/Leftcontent.jsx
+++ src/components/main-content/Leftcontent.jsx
@@ -1,16 +1,41 @@
 import React from "react";
 import "./maincontentstyle.css";
+import { leftcontentdata } from "../../db.js";
+import { IoIosSearch } from "react-icons/io";
 
 function Leftcontent() {
   return (
-    <div className="left-content-title">
-      <div className="left-content-info">
-        <p>Фильтрация по направлениям</p>
+    <>
+      <div className="left-content-title">
+        <div className="left-content-info">
+          <p>
+            Фильтрация <br /> по направлениям
+          </p>
+        </div>
+        <div className="filter-delete">
+          <span>X Сбросить</span>
+        </div>
       </div>
-      <div className="filter-delete">
-        <span>X Сбросить</span>
+      <div className="search-content">
+        <input type="text" placeholder="Поиск направления" />
+        <div className="search-content-icon">
+          <IoIosSearch />
+        </div>
       </div>
-    </div>
+      {leftcontentdata.map((value) => {
+        return (
+          <div className="checkbox-content">
+            <div className="checkbox-content-input">
+              <input type="checkbox" />
+            </div>
+            <h5>{value.title}</h5>
+            <div className="checkbox-content-number">
+              <span>({value.number})</span>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
--- src/db.js
+++ src/db.js
@@ -5,11 +5,89 @@ export const footerdata = [
       title: "MedYordam 2024 - Все права защищены"
   },
   {
-      id: 1,
+      id: 2,
       title: "Обработка данных"
   },
   {
-      id: 1,
+      id: 3,
       title: "Условия использования"
   }
+]
+
+export const leftcontentdata = [
+    {
+        id: 1,
+        title: "Аллерголог",
+        number: "12 312"
+    },
+    {
+        id: 2,
+        title: "Андролог",
+        number: "12 312"
+    },
+    {
+        id: 3,
+        title: "Венеролог",
+        number: "12 312"
+    },
+    {
+        id: 4,
+        title: "Врач УЗИ",
+        number: "12 312"
+    },
+    {
+        id: 5,
+        title: "Гастроэтеролог",
+        number: "12 312"
+    },
+    {
+        id: 6,
+        title: "Гематолог",
+        number: "12 312"
+    },
+    {
+        id: 7,
+        title: "Генетик",
+        number: "12 312"
+    },
+    {
+        id: 8,
+        title: "Гинеколог",
+        number: "12 312"
+    },
+    {
+        id: 9,
+        title: "Дерматолог",
+        number: "12 312"
+    },
+    {
+        id: 10,
+        title: "Диетолог",
+        number: "12 312"
+    },
+    {
+        id: 11,
+        title: "Инфекционист",
+        number: "12 312"
+    },
+    {
+        id: 12,
+        title: "Кардиолог",
+        number: "12 312"
+    },
+    {
+        id: 13,
+        title: "ЛОР",
+        number: "12 312"
+    },
+    {
+        id: 14,
+        title: "Маммолог",
+        number: "12 312"
+    },
+    {
+        id: 15,
+        title: "Нарколог",
+        number: "12 312"
+    },
 ]
